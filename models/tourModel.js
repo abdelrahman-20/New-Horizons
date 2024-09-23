db6906bdc6b57b5890b90ded1124e464a874f45e
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-09-23 22:39:29 +0300

    Add Virtual Data To The DB Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,63 +1,73 @@
 const mongoose = require('mongoose');
 
 // Creating Data Schema:
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: [true, 'Tour Name Must Be Unique'],
-    required: [true, 'Tour Must Have A Name'],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: [true, 'Tour Name Must Be Unique'],
+      required: [true, 'Tour Must Have A Name'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour Must Have A Duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour Must Have A Group Size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour Must Have A Difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour Must Have A Price'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Tour Must Have A Summary'],
+    },
+    description: {
+      type: String,
+      trime: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Tour Must Have A Cover Image'],
+    },
+    images: {
+      type: [String],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDate: {
+      type: [Date],
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, 'Tour Must Have A Duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour Must Have A Group Size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Tour Must Have A Difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour Must Have A Price'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Tour Must Have A Summary'],
-  },
-  description: {
-    type: String,
-    trime: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Tour Must Have A Cover Image'],
-  },
-  images: {
-    type: [String],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDate: {
-    type: [Date],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 // Create A Collection Using Previous Schema:
